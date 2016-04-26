e7cf463a2f24e2d4ce52795d445aea027616e901
Kyle Wolfson and Samuel Durfey <kyle@>
2016-04-26 11:45:44 -0700

    Time now passes on button press. Food, sleep and activity values decrease.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,6 +28,23 @@ public class App {
         request.session().attribute("myPet", myPet);
       }
       model.put("name", myPet.getName());
+      model.put("food", myPet.getFoodLevel());
+      model.put("sleep", myPet.getSleepLevel());
+      model.put("activity", myPet.getActivityLevel());
+
+      model.put("template", "templates/mypet.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/timepasses", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      Tamagotchi myPet = request.session().attribute("myPet");
+      myPet.timePasses();
+      model.put("name", myPet.getName());
+      model.put("food", myPet.getFoodLevel());
+      model.put("sleep", myPet.getSleepLevel());
+      model.put("activity", myPet.getActivityLevel());
 
       model.put("template", "templates/mypet.vtl");
       return new ModelAndView(model, layout);
